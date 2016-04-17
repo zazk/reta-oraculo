2a36689f57b987a92f45e9b1da246a5d9f391a80
Enrique Juan de Dios <juandedioz@gmail.com>
2016-04-17 10:41:20 -0500

    Upgrade Oraculo

---
+++ Reta al Oraculo/Reta al Oraculo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Reta al Oraculo
+//
+//  Created by user on 4/17/16.
+//  Copyright © 2016 cubix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.darkGrayColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
