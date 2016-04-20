318a1172d22748dd1df8ff72cecfd1cdb864e39c
Enrique Juan de Dios <juandedioz@gmail.com>
2016-04-19 19:51:06 -0500

    Add More Screens

--- Reta al Oraculo/Reta al Oraculo/ViewController.swift
+++ Reta al Oraculo/Reta al Oraculo/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg_choose")! )// UIImage(named: "bg_choose")
+        animate([ UIImage(named: "i-1")!, UIImage(named: "i-2")!, UIImage(named: "i-3")!, UIImage(named: "i-4")!, UIImage(named: "i-5")!, UIImage(named: "i-6")!])
+
+    }
+    
+    
+    func animate(images: [UIImage], index: Int = 0) {
+        print(index)
+        UIView.transitionWithView(imageView, duration: 1, options: .TransitionCrossDissolve, animations: {
+            self.imageView.image = images[index]
+            }, completion: { value in
+                let idx = index == images.count-1 ? 0 : index+1
+                self.animate(images, index: idx)
+        })
     }
 
     override func didReceiveMemoryWarning() {
